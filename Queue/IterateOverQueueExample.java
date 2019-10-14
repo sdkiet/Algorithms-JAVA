36b2c26436ac7b73ce9262abafb8e0826ac661b3
imshivamdwivedi <dwivedishivam758@gmail.com>
2019-10-14 23:56:03 +0530

    Queue & it's opeartion

---
+++ Queue/IterateOverQueueExample.java
@@ -0,0 +1,48 @@
+//Iterating over a Queue in Java
+
+//The example in this section shows various ways of iterating over a Queue:
+
+//    Iterate over a Queue using Java 8 forEach() method.
+  //  Iterate over a Queue using iterator().
+    //Iterate over a Queue using iterator() and Java 8 forEachRemaining() method.
+      // Iterate over a Queue using simple for-each loop.
+
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class IterateOverQueueExample {
+    public static void main(String[] args) {
+        Queue<String> waitingQueue = new LinkedList<>();
+
+        waitingQueue.add("John");
+        waitingQueue.add("Brad");
+        waitingQueue.add("Angelina");
+        waitingQueue.add("Julia");
+
+        System.out.println("=== Iterating over a Queue using Java 8 forEach() ===");
+        waitingQueue.forEach(name -> {
+            System.out.println(name);
+        });
+
+        System.out.println("\n=== Iterating over a Queue using iterator() ===");
+        Iterator<String> waitingQueueIterator = waitingQueue.iterator();
+        while (waitingQueueIterator.hasNext()) {
+            String name = waitingQueueIterator.next();
+            System.out.println(name);
+        }
+
+        System.out.println("\n=== Iterating over a Queue using iterator() and Java 8 forEachRemaining() ===");
+        waitingQueueIterator = waitingQueue.iterator();
+        waitingQueueIterator.forEachRemaining(name -> {
+            System.out.println(name);
+        });
+
+        System.out.println("\n=== Iterating over a Queue using simple for-each loop ===");
+        for(String name: waitingQueue) {
+            System.out.println(name);
+        }
+    }
+}
+
