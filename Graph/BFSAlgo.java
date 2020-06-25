7c366d404eb98b03ee11c0ce65149a5c8119d980
imshivamdwivedi <dwivedishivam758@gmail.com>
2020-06-25 19:33:58 +0530

    BFS Add

--- Graph/BFSAlgo.java
+++ Graph/BFSAlgo.java
@@ -1,76 +1,78 @@
-import java.io.*; 
-import java.util.*; 
 
+import java.util.*;
 
-class Graph 
-{ 
-	private int V; // No. of vertices 
-	private LinkedList<Integer> adj[]; //Adjacency Lists 
+class Graph {
+    
+    // A utility function to add an edge in an
+    // undirected graph
+    static void addEdge(ArrayList<ArrayList<Integer> > adj,
+                        int u, int v)
+    {
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+    
+    // Function to perform BFS traversal of a Graph
+    static void BFS(ArrayList<ArrayList<Integer> > adj, int V)
+    {
+        // Initialize a boolean array
+        // to keep track of visited vertices
+        boolean visited[] =  new boolean[V+1];
+    
+        // Mark all vertices not-visited initially
+        for (int i = 1; i <= V; i++)
+            visited[i] = false;
+        
+        // Create a queue for BFS 
+        LinkedList<Integer> queue = new LinkedList<Integer>(); 
+    
+        // The start vertex or source vertex is 1
+        int s = 1;
+        
+        // Mark the current node as 
+        // visited and enqueue it 
+        visited[s]=true; 
+        queue.add(s);   
+        
+        while (queue.size() != 0) 
+        { 
+            // Dequeue a vertex from queue and print it 
+            s = queue.poll(); 
+            System.out.print(s+" "); 
+  
+            for (int i = 0; i < adj.get(s).size(); i++) {
+                
+                int newNode = adj.get(s).get(i);
+                // Check if it is not visited
+                if(visited[newNode] == false){}
+                    visited[newNode] = true;
+                    queue.add(newNode);
+                }
+            }
+        } 
+    }
+    
+    // Driver Code
+    public static void main(String[] args)
+    {
+        // Creating a graph with 6 vertices
+        int V = 6;
+        ArrayList<ArrayList<Integer> > adj 
+                    = new ArrayList<ArrayList<Integer> >(V+1);
+        
+        for (int i = 0; i < V+1; i++)
+            adj.add(new ArrayList<Integer>());
 
-	// Constructor 
-	Graph(int v) 
-	{ 
-		V = v; 
-		adj = new LinkedList[v]; 
-		for (int i=0; i<v; ++i) 
-			adj[i] = new LinkedList(); 
-	} 
-
-	// Function to add an edge into the graph 
-	void addEdge(int v,int w) 
-	{ 
-		adj[v].add(w); 
-	} 
-
-	// prints BFS traversal from a given source s 
-	void BFS(int s) 
-	{ 
-		// Mark all the vertices as not visited(By default 
-		// set as false) 
-		boolean visited[] = new boolean[V]; 
-
-		// Create a queue for BFS 
-		LinkedList<Integer> queue = new LinkedList<Integer>(); 
-
-		// Mark the current node as visited and enqueue it 
-		visited[s]=true; 
-		queue.add(s); 
-
-		while (queue.size() != 0) 
-		{ 
-			// Dequeue a vertex from queue and print it 
-			s = queue.poll(); 
-			System.out.print(s+" "); 
-
-			
-			Iterator<Integer> i = adj[s].listIterator(); 
-			while (i.hasNext()) 
-			{ 
-				int n = i.next(); 
-				if (!visited[n]) 
-				{ 
-					visited[n] = true; 
-					queue.add(n); 
-				} 
-			} 
-		} 
-	} 
-
-	// Driver method to 
-	public static void main(String args[]) 
-	{ 
-		Graph g = new Graph(4); 
-
-		g.addEdge(0, 1); 
-		g.addEdge(0, 2); 
-		g.addEdge(1, 2); 
-		g.addEdge(2, 0); 
-		g.addEdge(2, 3); 
-		g.addEdge(3, 3); 
-
-		System.out.println("Following is Breadth First Traversal "+ 
-						"(starting from vertex 2)"); 
-
-		g.BFS(0); 
-	} 
+        // Adding edges one by one
+        addEdge(adj, 1, 2);
+        addEdge(adj, 1, 3);
+        addEdge(adj, 2, 4);
+        addEdge(adj, 2, 5);
+        addEdge(adj, 3, 5);
+        addEdge(adj, 4, 5);
+        addEdge(adj, 4, 6);
+        addEdge(adj, 5, 6);
+        
+        BFS(adj, V);
+    }
 }
